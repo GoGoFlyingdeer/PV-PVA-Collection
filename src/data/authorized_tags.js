f37a6b964e539df390fd449a45283fe265293f88
Aurélien Tabard <aurelien@tabard.fr>
2021-07-01 15:48:42 +0200

    tag management

--- src/data/authorized_tags.js
+++ src/data/authorized_tags.js
@@ -1,7 +1,40 @@
 define({
-	authorizedTags: {
-		"type:system": {
-			"description": "paper describing a system"
-		}
-	}
+  authorizedTags: {
+    "type:system": {
+      description: "paper describing a system",
+    },
+    "definition:white_feiner": {
+      description: "paper describing a system",
+    },
+    "definition:own_definition": {
+      description: "paper describing a system",
+    },
+    "methodology:field_study": {
+      description: "paper describing a system",
+    },
+    "data:science": {
+      description: "paper describing a system",
+    },
+    "technology:AR": {
+      description: "paper describing a system",
+    },
+    "data:engineering": {
+      description: "paper describing a system",
+    },
+    "methodology:lab_study": {
+      description: "paper describing a system",
+    },
+    "visualization:custom": {
+      description: "paper describing a system",
+    },
+    "visualization:standard": {
+      description: "paper describing a system",
+    },
+    "visualization:N/A": {
+      description: "paper describing a system",
+    },
+    "": {
+      description: "paper describing a system",
+    },
+  },
 });
--- src/data/tag_categories.js
+++ src/data/tag_categories.js
@@ -1,7 +1,22 @@
 define({
-	tagCategories: {
-		"type": {
-			"description": "type of the paper"
-		}
-	}
+  tagCategories: {
+    type: {
+      description: "type of the paper",
+    },
+    definition: {
+      description: "defintion of situated visualization",
+    },
+    methodology: {
+      description: "methodology of the paper",
+    },
+    data: {
+      description: "type of data",
+    },
+    technology: {
+      description: "technology used",
+    },
+    visualization: {
+      description: "type of situated visualization",
+    },
+  },
 });
